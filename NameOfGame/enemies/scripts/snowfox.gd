44a1c588df2a89cc8a42ee0c0b79120407ae809a
Suus Honkoop <suus.honkoop@student.hu.nl>
2026-01-08 11:33:24 +0100

    adding snowfox to ice one

---
+++ NameOfGame/enemies/scripts/snowfox.gd
@@ -0,0 +1,13 @@
+extends SmallEnemyBase
+
+func _ready():
+	super()
+	
+	if enemy_hud_scene:
+		enemy_hud = enemy_hud_scene.instantiate()
+		get_tree().current_scene.add_child.call_deferred(enemy_hud)
+		enemy_hud.setup(HEALTH)
+
+func _process(_delta):
+	if enemy_hud:
+		enemy_hud.global_position = global_position + hud_offset
