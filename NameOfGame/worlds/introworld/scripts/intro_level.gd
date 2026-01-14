e00a11cb8eb54520f8c3b03e2857cf9a2dc4c613
Thuutje <thuutje2@gmail.com>
2026-01-14 12:06:23 +0100

    load the game
    load the game

--- NameOfGame/worlds/introworld/scripts/intro_level.gd
+++ NameOfGame/worlds/introworld/scripts/intro_level.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
-#@export var gold_coin_scene: PackedScene
-#@onready var gold_coin_spawn_points = $GoldCoinSpawns.get_children()
-#
-#func _ready() -> void:
-	#for gold_coin_marker in gold_coin_spawn_points:
-		#spawn_item(gold_coin_marker.global_position, gold_coin_scene)
-#
-#func spawn_item(pos: Vector2, scene: PackedScene) -> void:
-	#var item = scene.instantiate()
-	#add_child(item)
-	#item.global_position = pos
-	#item.player = $Player
+@onready var save_manager := get_node("/root/SaveManager")
+@onready var player := get_tree().get_first_node_in_group("player")
+const SAVE_KEY := "mygame_save_v1"
+
+func _ready():
+	if save_exists():
+		print("⏩ Save gevonden, terugladen...")
+		save_manager.load_game(player)
+		goto_saved_level()
+
+func save_exists() -> bool:
+	var json = JavaScriptBridge.eval("""localStorage.getItem("%s");""" % save_manager.SAVE_KEY)
+	return json != null and json != ""
+	
+func goto_saved_level():
+	var level_name = JSON.parse_string(JavaScriptBridge.eval("""localStorage.getItem("%s");""" % SAVE_KEY)).get("current_level", "intro_level")
+	if level_name != get_tree().current_scene.name:
+		get_tree().change_scene("res://levels/%s.tscn" % level_name)
