6e7bb6e3d4970b7e84774c083e6a3b0f61d357f3
Thuutje <thuutje2@gmail.com>
2026-01-16 10:44:14 +0100

    save options titlescreen
    replaced code from intro to screen

--- NameOfGame/titlescreen/title_screen.gd
+++ NameOfGame/titlescreen/title_screen.gd
@@ -2,14 +2,48 @@ extends Node2D
 
 @onready var start_button: Button = %start_button
 @onready var settings_button: Button = %settings
+@onready var save_manager := get_node("/root/SaveManager")
+
+const SAVE_KEY := "mygame_save_v1"
+
+var has_save := false
 
 func _ready() -> void:
+	has_save = save_exists()
+
+	if has_save:
+		start_button.text = "Ga verder"
+
 	start_button.pressed.connect(_on_start_button_pressed)
 
-# https://docs.godotengine.org/en/4.4/tutorials/inputs/inputevent.html
 func _unhandled_input(event):
 	if event.is_action_pressed("start_game"):
 		_on_start_button_pressed()
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://character-picker/scenes/character_picker.tscn")
+	if has_save:
+		print("⏩ Save gevonden, laden...")
+		goto_saved_level()
+	else:
+		get_tree().change_scene_to_file(
+			"res://character-picker/scenes/character_picker.tscn"
+		)
+
+func save_exists() -> bool:
+	var json = JavaScriptBridge.eval(
+		"""localStorage.getItem("%s");""" % SAVE_KEY
+	)
+	return json != null and json != ""
+
+func goto_saved_level():
+	var data = JSON.parse_string(
+		JavaScriptBridge.eval("""localStorage.getItem("%s");""" % SAVE_KEY)
+	)
+
+	if data == null:
+		return
+
+	var level_name = data.get("current_level", "intro_level")
+	get_tree().change_scene_to_file(
+		"res://levels/%s.tscn" % level_name
+	)
